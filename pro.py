2ba39de1a4f119d5afcd0cf9e6ebc2bf8ee47112
ZahraaCoder <96212566+ZahraaCoder@users.noreply.github.com>
2023-03-28 16:29:38 -0400

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,68 @@
+import cv2
+img=cv2.imread('solar-system.jpg')
+cv2.putText(img,
+            "Sun",
+            (40,30),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Mercury",
+            (120,180),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Venus",
+            (180,280),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Earth",
+            (280,160),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Mars",
+            (380,260),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Jupiter",
+            (560,40),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Saturn",
+            (750,310),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Uranus",
+            (960,120),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+cv2.putText(img,
+            "Neptune",
+            (1100,300),
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.4,
+            (255,255,255)
+            )
+
+cv2.imshow('output',img)
+cv2.waitKey(0)
